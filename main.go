196d712a15cd85070545660db718252e78af6e37
Gunjan Patel <patelgunjan5@gmail.com>
2015-09-05 23:31:17 -0700

    yelling service for sleepyheads

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
+package main
 
-/*   
-    Copyright (C) 2015  Gunjan Patel
+/*
+   Copyright (C) 2015  Gunjan Patel
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
+import (
+	_ "encoding/json"
+	_ "errors"
+	"log"
+	"net/http"
+	_ "strings"
 
+	"golang.org/x/net/context"
+
+	_ "github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	yell "github.com/gunjan5/Skywalker/yellingService"
+)
+
+func main() {
+	ctx := context.Background()
+	svc := stringService{}
+
+	countHandler := httptransport.Server{
+		Context:            ctx,
+		Endpoint:           yell.makeCountEndpoint(svc),
+		DecodeRequestFunc:  yell.decodeCountRequest,
+		EncodeResponseFunc: yell.encodeResponse,
+	}
+
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ yellingService/yell.go
@@ -0,0 +1,73 @@
+package yellingService
+
+/*
+   Copyright (C) 2015  Gunjan Patel
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	_ "strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+// StringService provides operations on strings.
+type StringService interface {
+	Count(string) int
+}
+
+type stringService struct{}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
+
+func decodeCountRequest(r *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+// ErrEmpty is returned when an input string is empty.
+var ErrEmpty = errors.New("empty string")
